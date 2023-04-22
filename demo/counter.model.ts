bce0402bbf440ced550ff662747a4d3fc75cbf54
Vitaliy Berekchiyan <v.berekchiyan@semrush.com>
2023-04-23 00:02:07 +0200

    updated svitore

--- demo/counter.model.ts
+++ demo/counter.model.ts
@@ -1,21 +1,24 @@
-import { State, Event } from "svitore";
+import { State, Event, ComputeState } from "svitore";
 
 const increment = new Event();
 const reset = new Event();
 const changeInput = new Event<string>();
 
-const countState = new State(0);
-const inputState = new State("");
+const state = new State({ count: 0, input: "" });
 
-increment.listen(() => {
-	countState.change((state) => state + 1);
+const countState = new ComputeState(state, ({ count }) => count);
+const inputState = new ComputeState(state, ({ input }) => input);
+
+increment.subscribe(() => {
+	state.change((state) => ({ ...state, count: state.count + 1 }));
 });
 
-changeInput.listen((inputValue) => inputState.set(inputValue));
+changeInput.subscribe((inputValue) => {
+	state.change((state) => ({ ...state, input: inputValue }));
+});
 
-reset.listen(() => {
-	countState.reset();
-	inputState.reset();
+reset.subscribe(() => {
+	state.reset();
 });
 
 export { increment, countState, reset, changeInput, inputState };
