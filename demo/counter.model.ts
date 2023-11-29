4e19fcc13cf15c41a4abef2f04742bbee647cefa
Vitaliy Berekchiyan <v.berekchiyan@semrush.com>
2023-11-29 17:56:44 +0100

    updated svitore

--- demo/counter.model.ts
+++ demo/counter.model.ts
@@ -1,12 +1,12 @@
 import { Svitore } from "svitore";
 
-const $module = Svitore.createModule("svitore react module");
+const $module = Svitore.Module("svitore react module");
 
-const increment = $module.createEvent();
-const reset = $module.createEvent();
+const increment = $module.Event();
+const reset = $module.Event();
 
 const countState = $module
-	.createState(3)
+	.State(3)
 	.changeOn(increment, (_, state) => state + 1)
 	.resetOn(reset);
 
